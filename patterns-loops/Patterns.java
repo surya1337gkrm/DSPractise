49010bc30c9c111a1d10329a3a485a31a2d4bb1b
surya1337gkrm <suryavenkatesh0@gmail.com>
2023-06-07 07:01:20 -0400

    Patterns-Loops Java

---
+++ patterns-loops/Patterns.java
@@ -0,0 +1,16 @@
+import com.surya.pattern.*;
+
+class Patterns {
+    public static void main(String[] args) {
+        // Pattern.printGrid(5);
+        // Pattern.printRightAngledTriangle(5);
+        // Pattern.printRightAngledTriangleWithNumbers(5);
+        // Pattern.printRightAngledTriangleWithRowNumber(6);
+        // Pattern.printReverseRightAngledTriangle(6);
+        // Pattern.printReverseRightAngledTriangleWithNumbers(8);
+        // Pattern.printTriangle(8);
+        // Pattern.printReverseTriangle(4);
+        Pattern.printRhombus(6);
+
+    }
+}
---
+++ patterns-loops/com/surya/pattern/Pattern.java
@@ -0,0 +1,152 @@
+package com.surya.pattern;
+
+public class Pattern {
+    public static void printGrid(int n) {
+        System.out.println("Printing Grid.");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void printRightAngledTriangle(int n) {
+        System.out.println("Right Angled triangle.");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void printRightAngledTriangleWithNumbers(int n) {
+        // printing same pattern using numbers
+        System.out.println("Right Angled triangle with numbers.");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void printRightAngledTriangleWithRowNumber(int n) {
+        System.out.println("Right Angled triangle with row number");
+        // same pattern but instead of printing column value, print row number
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void printReverseRightAngledTriangle(int n) {
+        System.out.println("Reverse Right Angle Triangle.");
+        // Printing reverse right angle triangle
+        for (int i = n; i >= 1; i--) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printReverseRightAngledTriangleWithNumbers(int n) {
+        System.out.println("Reverse Right Angle Triangle with numbers.");
+        // Same pattern using numbers
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void printTriangle(int n) {
+        System.out.println("Printing triangle");
+        // Triangle
+        for (int i = 1; i <= n; i++) {
+            // spaces
+            for (int j = 1; j < n - i + 1; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j < (2 * i); j++) {
+                System.out.print("*");
+            }
+            // spaces
+            for (int j = 1; j < n - i + 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void printReverseTriangle(int n) {
+        System.out.println("Printing Reverse Triangle");
+        // Reverse Triangle
+        for (int i = 0; i < n; i++) {
+            // spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 0; j < (2 * (n - i) - 1); j++) {
+                System.out.print("*");
+            }
+            // spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void printRhombus(int n) {
+        System.out.println("Printing a Rhombus.\n");
+        // Rhombus [ Upper Triangle + Lower triangle ]
+        // Upper
+        for (int i = 0; i < n; i++) {
+            // spaces
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            // spaces
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        // Lower
+        for (int i = 0; i < n; i++) {
+            // spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 0; j < 2 * (n - i) - 1; j++) {
+                System.out.print("*");
+            }
+            // spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+}

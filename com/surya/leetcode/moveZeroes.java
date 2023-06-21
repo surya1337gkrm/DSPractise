b9e22bb39271be065730517e3cbad34e89d3c481
surya1337gkrm <suryavenkatesh0@gmail.com>
2023-06-20 23:27:06 -0400

    Move Zeroes - Method 02

--- com/surya/leetcode/moveZeroes.java
+++ com/surya/leetcode/moveZeroes.java
@@ -13,5 +13,28 @@ public class moveZeroes {
             nums[count] = 0;
             count++;
         }
+
+        // Method02
+        // int j = -1;
+        // // find the first occurrence of 0.
+        // for (int i = 0; i < nums.length; i++) {
+        // if (nums[i] == 0) {
+        // j = i;
+        // break;
+        // }
+        // }
+        // // return if there are no 0s in the array.
+        // if (j == -1)
+        // return;
+        // int temp;
+        // // keep the first pointer(i) next to the first 0. and swap
+        // for (int i = j + 1; i < nums.length; i++) {
+        // if (nums[i] != 0) {
+        // temp = nums[i];
+        // nums[i] = nums[j];
+        // nums[j] = temp;
+        // j++;
+        // }
+        // }
     }
 }

a2edec72af4ed77d746976fcecda91414c81b798
surya1337gkrm <suryavenkatesh0@gmail.com>
2023-06-29 16:18:50 -0400

    Next Permutation

---
+++ com/surya/leetcode/nextPermutation.java
@@ -0,0 +1,46 @@
+package com.surya.leetcode;
+
+public class nextPermutation {
+    void reverseArray(int low, int high, int[] arr) {
+        int temp;
+        while (low < high) {
+            temp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+            low++;
+            high--;
+        }
+    }
+
+    public void next_Permutation(int[] nums) {
+        int index = -1;
+        int n = nums.length;
+
+        // updating the breakpoint index
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index == -1) {
+            // reverse the entire array if there's no break point
+            reverseArray(0, n - 1, nums);
+        } else {
+            int temp;
+            for (int i = n - 1; i >= index; i--) {
+                // find the number greater than index element and swap the elements.
+                if (nums[i] > nums[index]) {
+                    temp = nums[i];
+                    nums[i] = nums[index];
+                    nums[index] = temp;
+                    break;
+                }
+            }
+            // reverse the rest of the array from index+1 to end
+            reverseArray(index + 1, n - 1, nums);
+        }
+
+    }
+}

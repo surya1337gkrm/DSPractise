d5f9c0b045746c5e7d3077554d2c4f92df82657c
surya1337gkrm <suryavenkatesh0@gmail.com>
2023-07-07 01:13:05 -0400

    Valid Parentheses

---
+++ com/surya/leetcode/validParentheses.java
@@ -0,0 +1,31 @@
+package com.surya.leetcode;
+
+import java.util.Stack;
+
+public class validParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            switch (ch) {
+                case '(':
+                case '{':
+                case '[':
+                    stack.push(ch);
+                    break;
+                case ')':
+                    if (stack.isEmpty() || stack.pop() != '(')
+                        return false;
+                    break;
+                case '}':
+                    if (stack.isEmpty() || stack.pop() != '{')
+                        return false;
+                    break;
+                case ']':
+                    if (stack.isEmpty() || stack.pop() != '[')
+                        return false;
+                    break;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
